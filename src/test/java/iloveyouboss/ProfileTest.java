8efa5c1c25b31a0856ebf1748d389de4cf09df6d
jws1837 <jws1837@gmail.com>
2022-06-08 15:27:11 +0900

    현재 33p Junit기초와 AAA(Arrang-Action-Assert) 실행과 단언 추가.

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -1,21 +1,26 @@
 package iloveyouboss;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProfileTest {
 
   @Test
-  public void test() {
-    // 지원자의답안
+  public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+    // 지원자가 제출한 답안
     Profile profile = new Profile("카카오");
     Question question = new BooleanQuestion(1, "보너스 받고 싶니? ");
     Answer profileAnswer = new Answer(question, Bool.FALSE);
     profile.add(profileAnswer);
 
-    // 모범답안
+    // 회사가 추구하는 답안
     Criteria criteria = new Criteria();
     Answer criteriaAnswer = new Answer(question, Bool.TRUE);
     Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
     criteria.add(criterion);
+
+    boolean matches = profile.matches(criteria);
+
+    Assert.assertFalse(matches);
   }
 }
