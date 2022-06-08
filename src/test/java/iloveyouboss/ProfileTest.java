cca7cde957a65c6be9ea4856414c66750af38089
jws1837 <jws1837@gmail.com>
2022-06-08 16:51:09 +0900

    feat: @Before Method를 이용한 중복코드 제거

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -1,20 +1,28 @@
 package iloveyouboss;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProfileTest {
+  private Profile profile;
+  private BooleanQuestion question;
+  private Criteria criteria;
+
+  @Before
+  public void create() {
+    Profile profile = new Profile("카카오");
+    Question question = new BooleanQuestion(1, "보너스 받고 싶니? ");
+    Criteria criteria = new Criteria();
+  }
 
   @Test
   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
     // 지원자가 제출한 답안
-    Profile profile = new Profile("카카오");
-    Question question = new BooleanQuestion(1, "보너스 받고 싶니? ");
     Answer profileAnswer = new Answer(question, Bool.FALSE);
     profile.add(profileAnswer);
 
     // 회사가 추구하는 답안
-    Criteria criteria = new Criteria();
     Answer criteriaAnswer = new Answer(question, Bool.TRUE);
     Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
     criteria.add(criterion);
@@ -27,13 +35,10 @@ public class ProfileTest {
   @Test
   public void matchAnswersTrueForAnyDontCareCriteria() {
     // 지원자가 제출한 답안
-    Profile profile = new Profile("카카오");
-    Question question = new BooleanQuestion(1, "우유 먹나요?");
     Answer profileAnswer = new Answer(question, Bool.FALSE);
     profile.add(profileAnswer);
 
     // 회사가 추구하는 답안
-    Criteria criteria = new Criteria();
     Answer criteriaAnswer = new Answer(question, Bool.TRUE);
     Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
     criteria.add(criterion);
