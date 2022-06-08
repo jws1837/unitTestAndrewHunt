f8ef5241e7be4ed66e5a2b58aa76b0fe0977406e
jws1837 <jws1837@gmail.com>
2022-06-08 18:31:39 +0900

    feat: 부동소수점 테스트(주의사항포함)
    책 46p
    Cause: 부동소수점은 근사치로 나오는 것이기에 테스트하면 실패로 나온다.
    Solve : 오차범위(delta)를 사용한다.

--- src/test/java/iloveyouboss/AssertTest.java
+++ src/test/java/iloveyouboss/AssertTest.java
@@ -4,69 +4,74 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AssertTest {
-  private Account account;
+    private Account account;
 
-  class Account {
+    class Account {
 
-    int balance;
-    String name;
+        int balance;
+        String name;
 
-    Account(String name) {
-      this.name = name;
+        Account(String name) {
+            this.name = name;
+        }
+
+        void deposit(int dollars) {
+            balance += dollars;
+        }
+
+        void withdraw(int dollars) {
+            if (balance < dollars) {
+                throw new InsufficientFundsException("balance only " + balance);
+            }
+            balance -= dollars;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public boolean hasPositiveBalance() {
+            return balance > 0;
+        }
     }
 
-    void deposit(int dollars) {
-      balance += dollars;
+    @Before
+    public void createAccount() {
+        account = new Account("an account name");
     }
 
-    void withdraw(int dollars) {
-      if (balance < dollars) {
-        throw new InsufficientFundsException("balance only " + balance);
-      }
-      balance -= dollars;
+    class InsufficientFundsException extends RuntimeException {
+        public InsufficientFundsException(String message) {
+            super(message);
+        }
+
+        private static final long serialVersionUID = 1L;
     }
 
-    public String getName() {
-      return name;
+    @Test
+    public void hasPositiveBalance() {
+        account.deposit(50);
+        assertTrue(account.hasPositiveBalance());
     }
 
-    public int getBalance() {
-      return balance;
+    @Test
+    public void depositIncreasesBalance() {
+        int initialBalance = account.getBalance();
+        account.deposit(100);
+        assertTrue(account.getBalance() > initialBalance);
+        assertThat(account.getBalance(), equalTo(100));
     }
 
-    public boolean hasPositiveBalance() {
-      return balance > 0;
+    @Test
+    public void FloatPointTest() {
+//        assertThat(2.32 * 3, equalTo(6.96)); //fail
+        assertEquals(2.32 * 3, 6.96,0.000001); //success,delta는 오차
     }
-  }
-
-  @Before
-  public void createAccount() {
-    account = new Account("an account name");
-  }
-
-  class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-      super(message);
-    }
-
-    private static final long serialVersionUID = 1L;
-  }
-
-  @Test
-  public void hasPositiveBalance() {
-    account.deposit(50);
-    assertTrue(account.hasPositiveBalance());
-  }
-
-  @Test
-  public void depositIncreasesBalance() {
-    int initialBalance = account.getBalance();
-    account.deposit(100);
-    assertTrue(account.getBalance() > initialBalance);
-    assertThat(account.getBalance(), equalTo(100));
-  }
 }
