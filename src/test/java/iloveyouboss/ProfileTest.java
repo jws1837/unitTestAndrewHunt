966c66bd9f5a54ad340f36ae43aba5eae58e1243
jws1837 <jws1837@gmail.com>
2022-06-14 16:44:20 +0900

    feat: 4.4. 집중적인 단일목적테스트의 가치
    matches()함수에 다 몰아넣어서 분리했을 때의 반복적인 공통초기화에서 자유롭다.

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -5,41 +5,64 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ProfileTest {
-  private Profile profile;
-  private BooleanQuestion question;
-  private Criteria criteria;
-/***
- * iloveyouboss_13(책에서제공하는예제) 테스트코드 7개 포함된 버전수록되어있음.
- * */
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
 
-  @Before
-  public void create() {
-    Profile profile = new Profile("카카오");
-    Question question = new BooleanQuestion(1, "보너스 받고 싶니? ");
-    Criteria criteria = new Criteria();
-  }
+    /***
+     * iloveyouboss_13(책에서제공하는예제) 테스트코드 7개 포함된 버전수록되어있음.
+     * */
 
-  @Test
-  public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-    // 지원자가 제출한 답안
-    profile.add(new Answer(question, Bool.FALSE));
-    // 회사가 추구하는 답안
-    criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+    @Before
+    public void create() {
+        Profile profile = new Profile("카카오");
+        Question question = new BooleanQuestion(1, "보너스 받고 싶니? ");
+        Criteria criteria = new Criteria();
+    }
 
-    boolean matches = profile.matches(criteria);
+    @Test
+    public void matches() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got milk?");
 
-    Assert.assertFalse(matches);
-  }
+        // answers false when must-match criteria not met
+        profile.add(new Answer(question, Bool.FALSE));
+        Criteria criteria = new Criteria();
+        criteria.add(
+                new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
-  @Test
-  public void matchAnswersTrueForAnyDontCareCriteria() {
-    // 지원자가 제출한 답안
-    profile.add(new Answer(question, Bool.FALSE));
-    // 회사가 추구하는 답안
-    criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+        Assert.assertFalse(profile.matches(criteria));
 
-    boolean matches = profile.matches(criteria);
+        // answers true for any don't care criteria
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria = new Criteria();
+        criteria.add(
+                new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
-    Assert.assertTrue(matches);
-  }
+        Assert.assertTrue(profile.matches(criteria));
+    }
+
+    @Test
+    public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+        // 지원자가 제출한 답안
+        profile.add(new Answer(question, Bool.FALSE));
+        // 회사가 추구하는 답안
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+
+        boolean matches = profile.matches(criteria);
+
+        Assert.assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        // 지원자가 제출한 답안
+        profile.add(new Answer(question, Bool.FALSE));
+        // 회사가 추구하는 답안
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+
+        boolean matches = profile.matches(criteria);
+
+        Assert.assertTrue(matches);
+    }
 }
